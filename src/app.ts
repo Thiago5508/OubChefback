8d33a00eaf408624fcb5aa1d50603dd117f7ea13
deadman5508 <deadman5508@gmail.com>
2026-02-03 17:27:54 -0300

    Vercel upload test

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes.js";
+import dotenv from "dotenv";
 
-export const app = express();
+dotenv.config();
+
+ const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+export default app;
