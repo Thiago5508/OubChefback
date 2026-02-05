9dbda3eba6ec2b235a367a8df278f47f743944c0
deadman5508 <deadman5508@gmail.com>
2026-02-05 20:36:42 -0300

    Fiz Product

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-import { app } from '../src/app';
-import { connectDB } from '../src/database/mongodb';
-
-export default async function handler(req, res) {
-  await connectDB();
-  return app(req, res);
-}
